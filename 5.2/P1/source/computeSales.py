0c50aeeaf9b26830695ed16e869dd3bd0a536e0e
Kari Tsuki <peiblo_1472@live.com.mx>
2026-02-12 10:05:54 -0600

    feat: add program description and initial imports for computeSales

---
+++ 5.2/P1/source/computeSales.py
@@ -0,0 +1,13 @@
+"""
+computeSales.py
+
+Este programa calcula el ingreso total de ventas de una empresa
+a partir de un catálogo de precios (archivo JSON) y un registro
+de ventas (archivo JSON).
+"""
+
+
+import sys
+import json
+import time
+import os
