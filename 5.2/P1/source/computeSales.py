77d6807ddb8e8639035fcf130fcc8895b4fb6bce
Kari Tsuki <peiblo_1472@live.com.mx>
2026-02-12 12:49:40 -0600

    feat: implement functional computeSales.py. validaded to comply with Pylint and Flake8

--- 5.2/P1/source/computeSales.py
+++ 5.2/P1/source/computeSales.py
@@ -13,10 +13,16 @@ import time
 import os
 
 
-def main():
+def main():  # pylint: disable=too-many-locals, too-many-statements
+    """Función principal del programa    """
 
+    start_time = time.time()
     if len(sys.argv) != 3:
-        print("Uso: python computeSales.py <archivo_catalogo_productos.json> <archivo_registro_ventas.json>")
+        print(
+            "Uso: python computeSales.py "
+            "<archivo_catalogo_productos.json> "
+            "<archivo_registro_ventas.json>"
+        )
         sys.exit(1)
 
     product_file = sys.argv[1]
@@ -34,18 +40,59 @@ def main():
         with open(product_file, "r", encoding="utf-8") as f:
             product_data = json.load(f)
     except json.JSONDecodeError:
-        print(f"Error: El archivo {product_file} no tiene un formato JSON válido.")
+        print(
+            f"Error: El archivo {product_file}"
+            "no tiene un formato JSON válido."
+            )
         sys.exit(1)
 
     try:
         with open(sales_file, "r", encoding="utf-8") as f:
             sales_data = json.load(f)
     except json.JSONDecodeError:
-        print(f"Error: El archivo {sales_file} no tiene un formato JSON válido.")
+        print(
+            f"Error: El archivo {sales_file}"
+            "no tiene un formato JSON válido."
+            )
         sys.exit(1)
 
     print(f"{len(product_data)} productos cargados desde {product_file}")
     print(f"{len(sales_data)} registros de ventas cargados desde {sales_file}")
+
+    price_dict = {item["title"]: item["price"] for item in product_data}
+    total_sales = 0
+    errors = []
+
+    for sale in sales_data:
+        product_name = sale.get("Product")
+        quantity = sale.get("Quantity", 0)
+        if product_name not in price_dict:
+            errors.append(f"Producto desconocido, {product_name}")
+            continue
+        total_sales += price_dict[product_name] * quantity
+    print("\n--- Resultados de ventas ---")
+    print(f"Total de ventas: ${total_sales:.2f}")
+
+    if errors:
+        print("\nErrores encontrados:")
+        for e in errors:
+            print(f"- {e}")
+
+    end_time = time.time()
+    tiempo_ejecucion = end_time - start_time
+
+    result_file = "../results/SalesResults.txt"
+    with open(result_file, "w", encoding="utf8") as f:
+        f.write("--- Resultado de Ventas --- \n")
+
+        f.write(f"Total de ventas: ${total_sales}")
+        if errors:
+            f.write("Errores encontrados:\n")
+            for e in errors:
+                f.write(f"- {e}\n")
+
+        f.write(f"\nTiempo de ejecución: {tiempo_ejecucion:.2f} segundos\n")
+
 
 if __name__ == "__main__":
     main()
